76add3af97faf676371e4ad59184e7d6a260f217
Lokovšek <lokovsek.jure@gmail.com>
2019-11-05 09:52:57 +0100

    Prepared app to work with bluetooth.

--- exploring_flutter/lib/screens/bluetooth_screen.dart
+++ exploring_flutter/lib/screens/bluetooth_screen.dart
@@ -1,4 +1,3 @@
-import 'package:exploring_flutter/screens/bluetooth_screen.dart';
 import 'package:exploring_flutter/stateless_widgets/image_widget.dart';
 import 'package:exploring_flutter/utils/app_route_navigator.dart';
 import 'package:fimber/fimber.dart';
@@ -6,7 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'item_list_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class BluetoothScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,12 +13,12 @@ class HomeScreen extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.only(top: 50.0),
         children: <Widget>[
-          ImageWidget(120.0, 120.0, "assets/images/flutter.png"),
+          ImageWidget(120.0, 120.0, "assets/images/flutter_ble_lib_logo.png"),
           Center(
-            child: textTitle("Flutter App"),
+            child: textTitle("App"),
           ),
-          raisedButtonItemListScreen(context, "Open List Screen", ItemListScreen()),
-          raisedButtonWebSocketTest(context, "Open Bluetooth Screen", BluetoothScreen()),
+          raisedButtonItemListScreen(context, "Test Click 1"),
+          raisedButtonWebSocketTest(context, "Test Click 2"),
         ],
       ),
     );
@@ -29,36 +28,36 @@ class HomeScreen extends StatelessWidget {
     return Text(value,
         textDirection: TextDirection.ltr,
         style: TextStyle(
-        color: Colors.indigoAccent,
-        fontSize: 40.0,
-        fontFamily: "Roboto",
-        fontStyle: FontStyle.italic,
-        fontWeight: FontWeight.bold
-    ));
+            color: Colors.indigoAccent,
+            fontSize: 40.0,
+            fontFamily: "Roboto",
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold
+        ));
   }
 
-  RaisedButton raisedButtonItemListScreen(BuildContext context, String buttonName, StatefulWidget statefulWidget) {
+  RaisedButton raisedButtonItemListScreen(BuildContext context, String buttonName) {
     return RaisedButton(
       padding: EdgeInsets.only(left: 50.0, right: 50.0),
       color: Theme.of(context).primaryColorDark,
       textColor: Theme.of(context).primaryColorLight,
       child: Text(buttonName, textScaleFactor: 1.5),
       onPressed: () {
-        Fimber.d("Click: Goint to $buttonName");
-        AppNavigator().navigateToStatefulWidget(context, statefulWidget);
+        Fimber.d("Click: $buttonName");
+        // do stuff
       },
     );
   }
 
-  RaisedButton raisedButtonWebSocketTest(BuildContext context, String buttonName, StatelessWidget statelessWidget) {
+  RaisedButton raisedButtonWebSocketTest(BuildContext context, String buttonName) {
     return RaisedButton(
       padding: EdgeInsets.only(left: 50.0, right: 50.0),
       color: Theme.of(context).primaryColorDark,
       textColor: Theme.of(context).primaryColorLight,
       child: Text(buttonName, textScaleFactor: 1.5),
       onPressed: () {
-        Fimber.d("Click: Goint to $buttonName");
-        AppNavigator().navigateToStatelessWidget(context, statelessWidget);
+        Fimber.d("Click: $buttonName");
+        // do stuff
       },
     );
   }
@@ -66,3 +65,13 @@ class HomeScreen extends StatelessWidget {
 }
 
 
+//
+//
+//@override
+//  void dispose() {
+//   // inputController.dispose();
+//  //  channel.sink.close();
+//    super.dispose();
+//  }
+//
+//
--- exploring_flutter/lib/stateless_widgets/image_widget.dart
+++ exploring_flutter/lib/stateless_widgets/image_widget.dart
@@ -2,11 +2,17 @@ import 'package:exploring_flutter/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
+
+  final double width, height;
+  final String imagePath;
+
+  ImageWidget(this.width, this.height, this.imagePath);
+
   @override
   Widget build(BuildContext context) {
-    AssetImage assetImage = AppAssets().getImageAsset("assets/images/flutter.png");
-    Image image = Image(image: assetImage, width: 250.0, height: 250.0);
-    return Container(padding: EdgeInsets.only(top: 20.0), child: image);
+    AssetImage assetImage = AppAssets().getImageAsset(imagePath); // "assets/images/flutter.png"
+    Image image = Image(image: assetImage, width: width, height: height);
+    return Container(child: image);
   }
 
 }
